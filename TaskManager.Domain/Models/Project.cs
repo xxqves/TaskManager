3eb9bc6567c1a44d6763664dcf24e366dab42a26
Imcxld <teoamiyune.work@gmail.com>
2026-04-21 01:39:34 +0300

    add new Domain model Project and enum TaskStatus

---
+++ TaskManager.Domain/Models/Project.cs
@@ -0,0 +1,30 @@
+﻿namespace TaskManager.Domain.Models
+{
+    public class Project
+    {
+        public const int MAX_PROJECTNAME_LENGTH = 50;
+
+        public Guid Id { get; }
+
+        public string Name { get; } = string.Empty;
+
+        public Guid OwnerId { get; }
+
+        private Project(Guid id, string name, Guid ownerId)
+        {
+            Id = id;
+            Name = name;
+            OwnerId = ownerId;
+        }
+
+        public static Project Create(Guid id, string name, Guid ownerId)
+        {
+            if (name.Length > MAX_PROJECTNAME_LENGTH)
+            {
+                throw new ArgumentException($"Username must not exceed {MAX_PROJECTNAME_LENGTH} characters.");
+            }
+
+            return new Project(id, name, ownerId);
+        }
+    }
+}
