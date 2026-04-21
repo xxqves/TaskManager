7a2229206784afbff5712d65c0c1d16c8ddfa461
Imcxld <teoamiyune.work@gmail.com>
2026-04-22 02:33:07 +0300

    add repository interfaces

---
+++ TaskManager.Application/Interfaces/IProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskManager.Domain.Models;
+
+namespace TaskManager.Application.Interfaces
+{
+    public interface IProjectRepository
+    {
+        Task<List<Project>> GetUserProjectsAsync(Guid userId, CancellationToken cancellationToken = default);
+
+        Task<Project> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+        Task<Guid> AddAsync(Project project, CancellationToken cancellationToken = default);
+    }
+}
