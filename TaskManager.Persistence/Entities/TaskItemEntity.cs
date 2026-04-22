bd0292b928212a6458f6c053bddfa7cabd35adae
Imcxld <teoamiyune.work@gmail.com>
2026-04-23 01:49:36 +0300

    add appdbcontext

---
+++ TaskManager.Persistence/Entities/TaskItemEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskManager.Persistence.Entities
+{
+    public class TaskItemEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; } 
+
+        public Guid ProjectId { get; set; }
+
+        public Guid AssignedUserId { get; set; }
+
+        public TaskStatus Status { get; set; }
+    }
+}
