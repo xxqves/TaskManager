cf5673d8872554d85583f9e7e83d6271496c753a
Imcxld <teoamiyune.work@gmail.com>
2026-04-26 20:17:17 +0300

    fix configuration

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -10,7 +10,7 @@ namespace TaskManager.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Configuration.AddConfiguration(builder.Configuration);
+            builder.Services.AddConfiguration(builder.Configuration);
 
             var app = builder.Build();
 
