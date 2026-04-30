bd6e4ad4e7eff7a8a0152cfd36b0ead7beffae1f
Imcxld <teoamiyune.work@gmail.com>
2026-04-30 20:24:07 +0300

    redesign interface IProjectRepository

--- TaskManager.Application/Interfaces/IProjectRepository.cs
+++ TaskManager.Application/Interfaces/IProjectRepository.cs
@@ -4,10 +4,12 @@ namespace TaskManager.Application.Interfaces
 {
     public interface IProjectRepository
     {
-        Task<List<Project>> GetUserProjectsAsync(Guid userId, CancellationToken cancellationToken = default);
-
-        Task<Project> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-
         Task<Guid> AddAsync(Project project, CancellationToken cancellationToken = default);
+
+        Task<List<Project>> GetProjectsAsync(CancellationToken cancellationToken = default);
+
+        Task<Guid> UpdateAsync(Guid id, CancellationToken cancellationToken = default);
+
+        Task<Guid> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
     }
 }
