4af3a6a49d20a33fdda3db0a3700c5b2f799dd3c
Imcxld <teoamiyune.work@gmail.com>
2026-04-30 19:15:02 +0300

    add new interface IProjectService

---
+++ TaskManager.Application/Interfaces/IProjectService.cs
@@ -0,0 +1,16 @@
+﻿using TaskManager.Application.Contracts.Project;
+using TaskManager.Domain.Models;
+
+namespace TaskManager.Application.Interfaces
+{
+    public interface IProjectService
+    {
+        Task<Guid> CreateAsync(CreateProjectRequest request, CancellationToken cancellationToken = default);
+
+        Task<List<Project>> GetMyAsync(CancellationToken cancellationToken = default);
+
+        Task<Guid> UpdateAsync(Guid id, UpdateProjectRequest request, CancellationToken cancellationToken = default);
+
+        Task<Guid> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
