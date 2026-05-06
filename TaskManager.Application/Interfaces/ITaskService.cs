efcf53f30f6f324f7077d02a8cdc98e84f08e60d
Imcxld <teoamiyune.work@gmail.com>
2026-05-06 23:58:48 +0300

    add interface ITaskService

---
+++ TaskManager.Application/Interfaces/ITaskService.cs
@@ -0,0 +1,18 @@
+﻿using TaskManager.Application.Contracts.TaskItem;
+using TaskManager.Domain.Models;
+
+namespace TaskManager.Application.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<Guid> CreateAsync(CreateTaskRequest request, CancellationToken cancellationToken = default);
+
+        Task<List<TaskItem>> GetByProjectAsync(Guid projectId, CancellationToken cancellationToken = default);
+
+        Task<Guid> UpdateAsync(UpdateTaskRequest request, CancellationToken cancellationToken = default);
+
+        Task<Guid> ChangeStatusAsync(Guid id, CancellationToken cancellationToken = default);
+
+        Task<Guid> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
